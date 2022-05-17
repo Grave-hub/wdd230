a51f3a92f561370be57ca5b9f2011a9727624a49
Grave-hub <har22003@byui.edu>
2022-05-16 20:21:41 -0700

    week 4 fixes

--- chamber/js/datetoday.js
+++ chamber/js/datetoday.js
@@ -1,3 +1,3 @@
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const today  = new Date();
-document.getElementById("datetoday").textContent = (today.toLocaleDateString("en-US", options));
+document.getElementById("datetoday").textContent = (today.toLocaleDateString("en-Uk", options));
