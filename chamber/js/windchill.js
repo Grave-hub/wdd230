5057384aa270d77244848163b34d1b1a0b2f3e81
Grave-hub <har22003@byui.edu>
2022-05-27 13:35:28 -0700

    js fixes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,19 @@
-document.getElementById('temp').value = temperature;
-document.getElementById('windS').value = windSpeed;
+const tempSpan = document.getElementById('temp');
+const windSpan = document.getElementById('windS');
+
+const temperature = tempSpan.textContent;
+const windSpeed = windSpan.textContent;
+
+function chillFunction(temperature, windSpeed) {
+    const w = parseInt(windSpeed);
+    const t = parseFloat(temperature);
+    return (35.74 + (0.6215 * t) - (35.75 * (Math.pow(w, .16))) + (.4275 * (t * (Math.pow(w, .16)))));
+} 
+let windChill = chillFunction(temperature,windSpeed).toFixed(1);
 
 if (temperature <= 50 && windSpeed > 3.0) {
-    function windChill(temperature, windSpeed) {
-        return 35.74 + (0.6215 * temperature) - (35.75 *(windSpeed ^ 0.16)) + (.4275(temperature * (windSpeed ^ 0.16)));
-    }
-    document.getElementById("windC").innerHTML = windChill;
+    const element = document.getElementById("windC")
+    element.textContent = windChill;
 } else {
     document.getElementById("windC").innerHTML = "N/A";
 }
