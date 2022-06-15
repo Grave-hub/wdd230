8ea1c9673d5343980542832870fa0a2e5bd0b593
Grave-hub <har22003@byui.edu>
2022-06-14 22:30:56 -0700

    week 8 fixes

---
+++ js/prophets.js
@@ -0,0 +1,18 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    prophets.forEach(displayProphets);
+  });
+const prophets = jsonObject['prophets'];
+function displayProphets(prophet) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    
+} 
