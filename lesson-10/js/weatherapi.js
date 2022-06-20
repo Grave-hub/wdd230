7fb683951a3685bb1f0b63b3760afb70604ec32c
Grave-hub <har22003@byui.edu>
2022-06-19 22:45:56 -0700

    week 10 weather api

---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,14 @@
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=6a3d5f69ba51cabc60170ae98d64a69f"
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+fetch(weatherURL)
+  .then((response) => response.json())
+  .then((data) => {
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  });
