dc3efc39f2be4982848e9449d0fb92ef5461a57e
Grave-hub <har22003@byui.edu>
2022-07-09 00:38:54 -0700

    footer fixes

--- w12final-project/js/weatherGet.js
+++ w12final-project/js/weatherGet.js
@@ -1,20 +1,25 @@
 const projLat = 38.984722
 const projLon = -77.113056
-const getWeatherURL = `"api.openweathermap.org/data/2.5/forecast?lat=${projLat}&lon=${projLon}&appid=6a3d5f69ba51cabc60170ae98d64a69f"`;
+const getWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${projLat}&lon=${projLon}&units=imperial&appid=6a3d5f69ba51cabc60170ae98d64a69f`;
 
 const currentTemp = document.querySelector('#temp');
 const weatherIcon = document.querySelector('#iconWeather');
 const captionFig = document.querySelector('figcaption');
-const windSpeedAPI = document.querySelector('#windS')
+const windSpeedAPI = document.querySelector('#windspeedMph')
+const currentHumidity = document.querySelector('#currentHumidity')
+const weatherAlert = document.querySelector('.weatherAlert')
 
 fetch(getWeatherURL)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    // currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-    // windSpeedAPI.innerHTML = `<strong>${data.wind.speed.toFixed(0)}`
-    // const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    // const desc = data.weather[0].description;
-    // weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
-    // captionFig.textContent = desc;
+    weatherAlert.innerHTML = `<strong>${data.weather[0].main}</strong>`;
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    windSpeedAPI.innerHTML = `<strong>${data.wind.speed.toFixed(0)}`
+    currentHumidity.innerHTML = `<strong>${data.main.humidity}</strong>`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);weatherIcon.setAttribute('alt', desc);
+    captionFig.textContent = desc;
+
   });
