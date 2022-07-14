9bd550b7f287dcbecef1f88a603d45aee8cf1f23
Grave-hub <har22003@byui.edu>
2022-07-13 23:01:41 -0700

    temples page

--- w12final-project/js/templeCarousel.js
+++ w12final-project/js/templeCarousel.js
@@ -26,10 +26,10 @@ function buildtmplCard(temple){
         cardImg.setAttribute('alt', `image of the ${temple.name}`);
         cardImg.setAttribute('loading', 'lazy');
         tmplName.innerHTML = temple.name;
-        dedicated.innerHTML = `Dedicated: ${temple.dedicationDate}`;
+        dedicated.innerHTML = `<bold>Dedicated:</bold> ${temple.dedicationDate}`;
         tmplAddress.innerHTML = `Address: ${temple.addresses}`;
         tmplPhonenum.innerHTML = `Phone number: ${temple.templePhone}`;
-        closedDates.innerHTML = temple.templeClosed;
+        closedDates.innerHTML = `Dates Closed: <br>${temple.templeClosed}`;
         
         sessionLinks.setAttribute('href', temple.ordinanceSchedule);
         sessionLinks.innerHTML = 'Session schedule';
@@ -48,34 +48,66 @@ function buildtmplCard(temple){
         });
 
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  firstTemple(slideIndex);
+}
+
+function firstTemple(slideIndex) {
+    fetch(carouselData)
+    .then((response) => response.json())
+    .then((temples) => {
+        templeNum = slideIndex-1;
+        const selectedTemple = temples.templeInfo[templeNum];
+        let currentImg = document.querySelector(`.myslidesImg${slideIndex}`);
+        let currentText = document.querySelector(`.slidestext${slideIndex}`);
+        let currentDedict = document.querySelector(`#dedicated${slideIndex}`);
+        currentImg.setAttribute('src', selectedTemple.imgSource);
+        currentImg.setAttribute('loading', "lazy");
+        currentImg.setAttribute('alt', `The ${selectedTemple.name}`);
+        currentText.innerHTML = selectedTemple.name;
+        currentDedict.innerHTML = `Dedicated: ${selectedTemple.dedicationDate}`;
+    });
+}
+
+const gridbutton = document.querySelector("#cardsGrid");
+const listbutton = document.querySelector("#cardsList");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
+
+stars.forEach((star, index) => {
+  star.addEventListener('click', () => {
+    let currentSlide = document.querySelector(`.slidestext${slideIndex}`);
+    let currentText = currentSlide.innerHTML;
+    localStorage.setItem( currentText, `was given ${index+1} star(s) `);
+    
+  })
+});
+
 getTemples();
-// let btnRight = document.querySelector("#rightarrow");
-// let btnLeft = document.querySelector("#leftarrow");
-
-// function firstTemple() {
-//     fetch(carouselData)
-//     .then((response) => response.json())
-//     .then((temples) => {
-//         let templeNum = 0;
-//         function up() {
-//             templeNum +=1;
-//         }
-//         function down() {
-//             templeNum -=1;
-//         }
-//         btnRight.addEventListener('click', (up));
-//         btnLeft.addEventListener('click', (down));
-
-//         console.log(templeNum);
-//         const firstTemple = temples.templeInfo[templeNum];
-//         document.getElementById("carouselTemplate").src = firstTemple.imgSource;
-//         let templeName = document.querySelector(".temple-name");
-//         let templeLocation = document.querySelector(".temple-location");
-//         let templeDedication = document.querySelector(".temple-dedicated");
-//         templeName.innerHTML = firstTemple.name;
-//         templeLocation.innerHTML = firstTemple.location;
-//         templeDedication.innerHTML = firstTemple.dedicationDate;
-//     });
-// }
-
-// firstTemple();
