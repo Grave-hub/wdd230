621833d7c685479a279e88c294ee097b06f711dd
Grave-hub <har22003@byui.edu>
2022-07-19 23:46:27 -0700

    almost done

---
+++ w12final-project/js/popUp.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("popUpForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("popUpForm").style.display = "none";
+}
